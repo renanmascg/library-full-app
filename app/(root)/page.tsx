b8411c3b132537bf681116247e3b486f06776000
Renan Mascarenhas <renanmascg@gmail.com>
2025-02-19 00:38:18 -0300

    feat: Adiciona drizzle orm na aplicação, integra, faz a primeira migration e conecta com o NEON.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,17 +1,25 @@
 import BookList from "@/components/BookList";
 import BookOverview from "@/components/BookOverview";
-import { Button } from "@/components/ui/button";
 import { sampleBooks } from "@/constants";
-const Home = () => (
-  <>
-    <BookOverview {...sampleBooks[0]} />
+import { db } from "@/database/drizzle";
+import { usersTable } from "@/database/schema";
 
-    <BookList
-      title="Latest Books"
-      books={sampleBooks}
-      containerClassName="mt-28"
-    />
-  </>
-);
+const Home = async () => {
+  const result = await db.select().from(usersTable);
+
+  console.log(JSON.stringify(result, null, 2));
+
+  return (
+    <>
+      <BookOverview {...sampleBooks[0]} />
+
+      <BookList
+        title="Latest Books"
+        books={sampleBooks}
+        containerClassName="mt-28"
+      />
+    </>
+  );
+};
 
 export default Home;
