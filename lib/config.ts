ef8eb156eadb9e6aac2f2e39cfbc596529e55767
Renan Mascarenhas <renanmascg@gmail.com>
2025-02-20 12:48:43 -0300

    feat: Adiciona redis e ratelimiter dentro da aplicação com redis do upstash

--- lib/config.ts
+++ lib/config.ts
@@ -7,6 +7,10 @@ const config = {
     },
     apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT!,
     databaseUrl: process.env.DATABASE_URL,
+    upstash: {
+      redisUrl: process.env.UPSTASH_REDIS_URL,
+      redisToken: process.env.UPSTASH_REDIS_TOKEN,
+    },
   },
 };
 
